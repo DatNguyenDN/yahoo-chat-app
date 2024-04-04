bc87d53447258616a8880d8f6303a4f6ee2743d4
Dat Nguyen <ncdat2610@gmail.com>
2024-04-04 17:45:22 +0700

    add vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: "src",
+  build: {
+    outDir: "../dist",
+  },
   plugins: [react()],
   server: {
     port: 3000,
