3f3715fb50c036fa435b21e0db286d20318e3667
Dat Nguyen <ncdat2610@gmail.com>
2024-04-04 16:49:34 +0700

    ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,10 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
-
+import path from "path";
 const PORT = process.env.PORT || 8000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -16,9 +18,10 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("/", (req, res) => {
-  res.send("hello weoooo!!!!!!");
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 server.listen(PORT, () => {
